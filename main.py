5ca2fd84faed32d9c593d02c663387927109ce3d
mikiokubo <kubo@kaiyodai.ac.jp>
2020-11-13 11:03:49 +0900

    after lecture

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import streamlit as st
 import pandas as pd 
 import plotly.express as px
 
-st.title("Test App")
-st.write("Welcome")
+st.title("Covid-19 Analytics")
+st.subheader("コロナの死者数のデモ")
+
 df = pd.read_csv("COVID-19.csv")
 df["date"] = pd.to_datetime(df.dateRep)
 
-st.write(df)
+st.write(df.head())
+
 fig = px.line(df,x="date",y="deaths",color="countriesAndTerritories")
 st.write(fig)
